e46665296d96e9c854c8ad88c46db336dd2b32f6
Alex Bonwit <alexbonwit@mac.com>
2020-01-06 13:39:20 -0600

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const hours = time.split(":");
+  const parsedHours = parseInt(hours[0]);
+  if (parsedHours < 12) {
+    return "Good Morning";
+  }
+  if (parsedHours >= 12 && parsedHours <= 17) {
+    return "Good Afternoon";
+  }
+  if (parsedHours > 17) {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
