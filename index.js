e906d1fbff12f514a5daf30261aa65649248269f
github username <lluisguardiola.h@gmail.com>
2020-01-07 07:06:48 -0600

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let hour = Number(time.slice(0, 2));
+  // console.log(NaN);
+  // console.log(hour);
+  // console.log(hour === NaN);
+  // console.log("=======");
+  if (hour < 12 || isNaN(hour)) {
+    return 'Good Morning';
+  } else if (hour < 17) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  document.getElementsByTagName('h1')[0].innerText = message
+}
